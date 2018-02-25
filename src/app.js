eac4bc3a267eb44b2bd6b93f470879e0a35eec08
ly2011 <1518550424@qq.com>
2018-02-25 22:28:10 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,64 @@
+import http from 'http';
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import json from 'koa-json';
+import logger from 'koa-logger';
+
+const app = new Koa();
+const router = new Router();
+
+// 中间件
+app.use(bodyParser());
+app.use(json());
+app.use(logger());
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+router.get('/', async (ctx, next) => {
+  ctx.body = {
+    code: 200,
+    message: '请求成功',
+  };
+});
+app.use(router.routes()).use(router.allowedMethods());
+
+// error logger
+app.on('error', async (err, ctx) => {
+  console.log('error occured: ', err);
+});
+
+const port = 3000;
+const server = http.createServer(app.callback());
+server.listen(port);
+
+server.on('error', (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${port} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${port} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+server.on('listening', () => {
+  console.log('Listening on port: %d', port);
+});
+
+export default app;
